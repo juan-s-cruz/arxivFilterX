d71a5c3880c684cb7c27907a81842d70e7fe7f3b
juan-s-cruz <jscruz19@gmail.com>
2025-08-17 21:44:11 +0200

    llm_connector uses local model and provides topic explanations

--- llm_search/llm_connector.py
+++ llm_search/llm_connector.py
@@ -1,22 +1,57 @@
+import os
 import logging as log
 
-logging = log.getLogger(__name__)
-
+import torch
+import transformers
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 from huggingface_hub import InferenceClient
 
-import os
 
-client = InferenceClient(
-    api_key=os.environ["HF_TOKEN"],
-    # provider="auto",   # Automatically selects best provider
+logging = log.getLogger(__name__)
+transformers.logging.set_verbosity_error()
+
+if torch.cuda.is_available():
+    torch.device("cuda:0")
+
+model_location = "llm_search/models"
+
+tokenizer = AutoTokenizer.from_pretrained(model_location)
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_location, torch_dtype=torch.float16, device_map="auto"
 )
 
-# Chat completion
-response = client.chat.completions.create(
-    model="microsoft/phi-4",
-    messages=[{"role": "user", "content": "A story about hiking in the mountains"}],
+
+system_role = """
+You are an expert physicist and can elaborate on any topic related to physics. 
+Given a subfield of physics, you will provide a detailed explanation of the topic in 
+a small text of maximum two paragraphs.
+"""
+
+pipe = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
 )
 
+generation_args = {
+    "max_new_tokens": 500,
+    "return_full_text": False,
+    # "temperature": 0.01,
+    "do_sample": False,
+}
 
-def call_llm():
-    return response.choices[0].message["content"]
+
+def call_llm(topic: str) -> str:
+    """
+    Call the LLM with a given topic and return the generated text.
+
+    Args:
+        topic (str): The topic to generate text about.
+    """
+    messages = [
+        {"role": "system", "content": system_role},
+        {"role": "user", "content": topic},
+    ]
+    output = pipe(messages, **generation_args)
+    return output[-1]["generated_text"]
